577ea81b2b9e72e6cc312677b6679aa797a6b0c5
artiepoole <stuart.poole@canonical.com>
2025-05-22 13:11:26 +0100

    flag support using clap

--- rusty-list/src/main.rs
+++ rusty-list/src/main.rs
@@ -1,19 +1,44 @@
-use std::{env, fs};
+use std::{fs};
 use std::path::PathBuf;
+use clap::{Parser};
 
+#[derive(Parser)]
+#[command(version, about, long_about = None)]
+struct Cli {
+    // optional positional
+    /// The starting directory for search
+    #[arg(default_value = "./")]
+    path: Option<PathBuf>,
 
+    // optional flags
+    /// Use a long listing format
+    #[arg(short)]
+    long_mode: bool,
+    /// Do not ignore entries starting with .
+    #[arg(short, long)]
+    all: bool,
+    /// List all entries in directory order
+    #[arg(short = 'f')]
+    directory_order: bool,
+    /// List subdirectories recursively
+    #[arg(short='R', long)]
+    recursive: bool,
 
+}
 fn main() {
-    let mut args: Vec<String> = env::args().collect();
-    // remove first argument which is self
-    args.remove(0);
-    let path = PathBuf::from(args.pop().unwrap_or_else(|| "./".to_owned()));
-    if !path.exists(){panic!("path doesn't exist")}
+    let args = Cli::parse();
+
+    println!("long mode: {:?}", args.long_mode);
+    println!("all mode: {:?}", args.all);
+    println!("file order: {:?}", args.directory_order);
+    println!("recursive mode: {:?}", args.recursive);
+
+    let path = args.path.unwrap();
+    if !path.exists(){panic!("path '{}' doesn't exist", path.display());}
     let paths = fs::read_dir(path).unwrap();
     for path in paths {
         println!("{}", path.unwrap().path().display());
     }
 
-    // println!("path: {:?}", path.display());
 
 }
