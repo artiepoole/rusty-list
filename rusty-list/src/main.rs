4a68fa983f77c978c0517c4a32897743fdf810a8
artiepoole <stuart.poole@canonical.com>
2025-05-22 12:14:46 +0100

    parse first input

--- rusty-list/src/main.rs
+++ rusty-list/src/main.rs
@@ -1,3 +1,15 @@
+use std::env;
+use std::path::PathBuf;
+
 fn main() {
-    println!("Hello, world!");
+    let mut args: Vec<String> = env::args().collect();
+    // remove first argument which is self
+    args.remove(0);
+    let pathstr = args.pop().unwrap_or_else(|| "./".to_owned());
+
+
+    let path = PathBuf::from(pathstr);
+    
+    println!("path: {:?}", path.display());
+
 }
