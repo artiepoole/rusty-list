e61c13da1f910fb238320c19ccbb4618fc61c004
artiepoole <stuart.poole@canonical.com>
2025-05-22 12:20:26 +0100

    working path input, no support for flags or other inputs.

--- rusty-list/src/main.rs
+++ rusty-list/src/main.rs
@@ -1,12 +1,19 @@
-use std::env;
+use std::{env, fs};
 use std::path::PathBuf;
+
+
 
 fn main() {
     let mut args: Vec<String> = env::args().collect();
     // remove first argument which is self
     args.remove(0);
     let path = PathBuf::from(args.pop().unwrap_or_else(|| "./".to_owned()));
-    
-    println!("path: {:?}", path.display());
+    if !path.exists(){panic!("path doesn't exist")}
+    let paths = fs::read_dir(path).unwrap();
+    for path in paths {
+        println!("{}", path.unwrap().path().display());
+    }
+
+    // println!("path: {:?}", path.display());
 
 }
